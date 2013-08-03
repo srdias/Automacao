d20f366c956d9e72be3c2e0ab8d3feb48e1398a9
AdrianoNB <adrianomdias@gmail.com>
2013-08-02 22:33:02 -0300

    Revert "Teste"
    This reverts commit 6fc02fcc6f1de3933df8562b60355610465e9fda.

--- arduino/libraries/AdModulos/AdModulos.cpp
+++ arduino/libraries/AdModulos/AdModulos.cpp
@@ -34,15 +34,8 @@ void convBase32(int valor, char * retorno){
 	short fisrt=valor/32;
 	short last=valor%32;
 	char base32[] PROGMEM  = {"0123456789ABCDEFGHIJKLMNOPQRSTUV"};
-	char basen[] PROGMEM  = {"abcdefghij"};
-	
-	if(valor<10){
-		retorno[0]=basen[valor];
-		retorno[1]=0;
-	}else{
-		retorno[0]=base32[fisrt];
-		retorno[1]=base32[last];
-	}
+	retorno[0]=base32[fisrt];
+	retorno[1]=base32[last];
 }
 
 void addBase32ToString(char * a_string, int valor){
@@ -52,37 +45,43 @@ void addBase32ToString(char * a_string, int valor){
 	
 	convBase32(valor, lsconvertido);
 	
-	a_string[len++]=lsconvertido[0];
-	a_string[len++]=lsconvertido[1];
-	a_string[len]=0;
+	a_string[len]=lsconvertido[0];
+	a_string[len+1]=lsconvertido[1];
+	a_string[len+2]=0;
 
 }
 
-void Modulo::publicarDebug(){
+short Modulo::publicar(){
+/*
+	short ctrl=0;
+	for(short i=0;i<this->iVarQtde;i++){
+		if(ctrl==0){
+			Serial.print("---");
+			Serial.println();
+			ctrl=1;
+		}
+		
+		publicarValor(i, this->iVariaveis[i] );
+	}
+*/
 	char * lsOut = new char[this->iVarQtde*2+4+1];
+	char * lsOut2 = new char[this->iVarQtde*2+4+1];
+	
+	lsOut2[0]=0;
+	addBase32ToString(lsOut2,iRegistro);
+	addBase32ToString(lsOut2,iTipoModulo);
 	
 	sprintf(lsOut,"%d;%d",iRegistro,iTipoModulo);
 	for(short i=0;i<this->iVarQtde;i++){
 		sprintf(&lsOut[strlen(lsOut)],";%d",this->iVariaveis[i]);
+		addBase32ToString(lsOut2,this->iVariaveis[i]);
 	};
 	sprintf(&lsOut[strlen(lsOut)],"|");
 	Serial.println(lsOut);
+	Serial.println(lsOut2);
 	delete lsOut;
-};
-
-void Modulo::publicar(){
-	char * lsOut = new char[this->iVarQtde*2+4+1];
-	
-	lsOut[0]=0;
-	addBase32ToString(lsOut,iRegistro);
-	addBase32ToString(lsOut,iTipoModulo);
-	
-	for(short i=0;i<this->iVarQtde;i++){
-		addBase32ToString(lsOut,this->iVariaveis[i]);
-	};
-	strcat(lsOut,"|");
-	Serial.print(lsOut);
-	delete lsOut;
+	delete lsOut2;
+	return 1;
 };
 
 short Modulo::varAlocar(short qtde){
