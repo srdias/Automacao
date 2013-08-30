9abd7431d140635cf5c08f2b7bb75bba6c4e65d9
Adriano-i5 <adrianomdias@gmail.com>
2013-08-30 13:49:52 -0300

    Antes alteracao unidades

--- WebServer/Apache2.2/htdocs/automacaoresidencial/index.php
+++ WebServer/Apache2.2/htdocs/automacaoresidencial/index.php
@@ -10,8 +10,14 @@ session_start();
 <link rel="stylesheet" href="style.css" type="text/css" media="screen">
 <script type="text/javascript" src="jquery-1.js"></script>
 <script type="text/javascript"  language="javascript">
-function loadContent(Div, sourceUrl) {
-$(Div).load(sourceUrl);
+function loadContent(sourceUrl) {
+	var endereco="teste01.php?menu=" + sourceUrl
+	
+	if( sourceUrl.substring(0, 6) == "_acao_"){
+		alert(sourceUrl);
+	}else{
+		$("#DivMenu").load(endereco);
+	}
 }
 
 function setTexto(texto) {
@@ -20,89 +26,22 @@ function setTexto(texto) {
 
 $(document).ready(function() {
 $("#linkAltera").click(function() {
-var str="teste";
 //str="XXX"+str+" Este eh o novo texto heehe!";
-if( $("#caixaTexto").html() == "SIM" ){
-	$("#caixaTexto").html("NAO");
-}else{
-	$("#caixaTexto").html("SIM");
-}
+loadContent('inicio')
 });
 });
 
 </script>
 
 <style type="text/css" >
-#container {
-        border:thin blue solid;
-		background-color:3399FF;
-		width: 100px; 
-		height: 100px; 
-}
-
-#DivMenu {
-        xborder:thin blue solid;
-		background-color:3399FF;
-		width: 90%; 
-		height: 80%; 
-}
 
 </style>
 
 </head>
-<body>
-
-<?php
-
-function setOpcao(&$opcoes,$sessao,$nome,$codigo){
-	$indice=count($opcoes[$sessao]);
-	$opcoes[$sessao][$indice][0] = $nome;
-	$opcoes[$sessao][$indice][1] = $codigo;
-}
-
-$opcoes = array();
-setOpcao($opcoes,"inicio","Cozinha","c1");
-setOpcao($opcoes,"inicio","Quarto Casal","q1");
-setOpcao($opcoes,"inicio","Quarto Diana","q2");
-setOpcao($opcoes,"inicio","Quarto Lucas","q3");
-setOpcao($opcoes,"inicio","Banheiro Social","b1");
-setOpcao($opcoes,"inicio","Banheiro Suite Casal","b2");
-
-setOpcao($opcoes,"c1","Lampadas Setor 1","_acao_c1-lampadas-s1");
-setOpcao($opcoes,"c1","Lampadas Setor 2","_acao_c1-lampadas-s2");
-setOpcao($opcoes,"c1","Micro ondas","_acao_c1-micro-ondas");
-setOpcao($opcoes,"c1","TV","_acao_c1-tv");
-
-setOpcao($opcoes,"q1","Lampadas","_acao_q1-lampadas");
-setOpcao($opcoes,"q1","Ar condicionado","_acao_q1-ar-condicionado");
-setOpcao($opcoes,"q1","TV","_acao_q1-tv");
+<body  onload="loadContent('inicio')">
 
 
-?> 
-
-<div id="DivMenu">
-<table>
-<?php
-
-if (!empty($_GET['menu'])){
-	$menu=$_GET['menu'];
-}else{
-	$menu='inicio';
-}
-
-for($i=0;$i<count($opcoes[$menu]); $i++) {
-	$descricao=$opcoes[$menu][$i][0];
-	$id_menu=$opcoes[$menu][$i][1];
-	echo "<tr> <td onclick=\"setTexto('$id_menu');\"> $descricao </td> </tr>";
-}
-?>
-</table>
-</div>
-
-<div id="container"></div>
-<a id="linkAltera">Alterar conteudo</a>
-<br/>
-<div id="caixaTexto">Este conteudo sera alterado para outra coisa qualquer</div>
+<div id="DivMenu"></div>
 
 </body>
 </html>
--- WebServer/Apache2.2/htdocs/automacaoresidencial/teste01.php
+++ WebServer/Apache2.2/htdocs/automacaoresidencial/teste01.php
@@ -8,10 +8,103 @@ if (!empty($_GET['menu'])){
 	$menu='inicio';
 }
 
-for($i=0;$i<count($opcoes[$menu]); $i++) {
-	$descricao=$opcoes[$menu][$i][0];
-	$id_menu=$opcoes[$menu][$i][1];
-	echo "<tr> <td onclick=\"setTexto('$id_menu');loadContent('?menu=$id_menu');\"> $descricao </td> </tr>";
+function setOpcao(&$opcoes,$sessao,$nome,$codigo){
+	$indice=count($opcoes[$sessao]);
+	$opcoes[$sessao][$indice][0] = $nome;
+	$opcoes[$sessao][$indice][1] = $codigo;
+}
+
+$opcoes = array();
+setOpcao($opcoes,"inicio","Cozinha","c1");
+setOpcao($opcoes,"inicio","Quarto Casal","q1");
+setOpcao($opcoes,"inicio","Quarto Diana","q2");
+setOpcao($opcoes,"inicio","Quarto Lucas","q3");
+setOpcao($opcoes,"inicio","Banheiro Social","b1");
+setOpcao($opcoes,"inicio","Banheiro Suite Casal","b2");
+
+setOpcao($opcoes,"c1","Lampadas Setor 1","_acao_c1-lampadas-s1");
+setOpcao($opcoes,"c1","Lampadas Setor 2","_acao_c1-lampadas-s2");
+setOpcao($opcoes,"c1","Micro ondas","_acao_c1-micro-ondas");
+setOpcao($opcoes,"c1","TV","_controle_tv_philco");
+
+setOpcao($opcoes,"q1","Lampadas","_acao_q1-lampadas");
+setOpcao($opcoes,"q1","Ar condicionado","_acao_q1-ar-condicionado");
+setOpcao($opcoes,"q1","TV","_acao_q1-tv");
+
+$titulo="Titulo do menu";
+?>
+
+<tr> 
+	<td class="opcaoTitulo" >
+		<table border=1 width="100%" >
+			<tr>
+				<td class="opcaoTitulo" width="3%" >
+					<img src="seta.png" width="40" height="40" onclick="loadContent('<?php echo "inicio"; ?>');">
+				</td>
+				<td class="opcaoTitulo" width="94%" >
+					<?php echo $titulo; ?>
+				</td>
+				<td class="opcaoTitulo" width="3%" >
+				</td>
+			</tr>
+		</table>
+	</td>
+</tr>
+
+<?php
+
+if( $menu != "_controle_tv_philco" ){
+
+	for($i=0;$i<count($opcoes[$menu]); $i++) {
+		$descricao=$opcoes[$menu][$i][0];
+		$id_menu=$opcoes[$menu][$i][1];
+		echo "<tr> <td class=\"opcao\" onclick=\"loadContent('$id_menu');\"> $descricao </td> </tr>";
+	}
+}else{
+?>
+
+<tr> 
+	<td>
+		<table class="controle" >
+			<tr>
+				<td class="controle">1</td>
+				<td class="controle">2</td>
+				<td class="controle">3</td>
+			</tr>
+			<tr>
+				<td class="controle">4</td>
+				<td class="controle">5</td>
+				<td class="controle">6</td>
+			</tr>
+			<tr>
+				<td class="controle">7</td>
+				<td class="controle">8</td>
+				<td class="controle">9</td>
+			</tr>
+			<tr>
+				<td class="controle">Mudo</td>
+				<td class="controle" >0</td>
+				<td class="controle" >Voltar</td>
+			</tr>
+			<tr>
+				<td class="controle">Vol +</td>
+				<td class="controle"></td>
+				<td class="controle">Canal +</td>
+			</tr>
+			<tr>
+				<td class="controle">Vol -</td>
+				<td class="controle"></td>
+				<td class="controle">Canal -</td>
+			</tr>
+		</table>
+	</td>
+</tr>
+
+<?php
 }
 ?>
+
 </table>
+
+
+
